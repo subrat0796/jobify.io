ad41d855277eaf3a06b4071e4b7519b20d1cdda6
subrat37 <subratsahoo9420@gmail.com>
2022-04-22 03:45:04 +0530

    Styled-components setup

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,49 @@
 import React from "react";
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import styled from "styled-components";
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh-var(--nav-height));
+    display: grid;
+    align-items: center;
+    justify-content: center;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="jobify.io" />
       </nav>
@@ -14,14 +53,14 @@ const Landing = () => {
           <h1>
             job <span>tracking</span> app
           </h1>
-          <p></p>
+          <p>Start tracing jobs before you loose track of them</p>
           <button to="/register" className="btn btn-hero">
             Login/Register
           </button>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
-    </main>
+    </Wrapper>
   );
 };
 
