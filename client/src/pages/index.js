6194d0c192d49722579f404decd6265dfbe8a27c
subrat0796 <subratsahoo0796@gmail.com>
2022-04-22 22:19:39 +0530

    Adding pages

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,7 @@
 import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import Register from "./Register";
+import Error from "./Error";
 
-export { Landing };
+export { Landing, Dashboard, Login, Register, Error };
