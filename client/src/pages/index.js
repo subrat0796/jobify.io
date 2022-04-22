e33a122e85d93c2a8d33650ed4d06e451647154f
subrat0796 <subratsahoo0796@gmail.com>
2022-04-22 22:36:06 +0530

    Working on register page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,6 @@
 import Landing from "./Landing";
 import Dashboard from "./Dashboard";
-import Login from "./Login";
 import Register from "./Register";
 import Error from "./Error";
 
-export { Landing, Dashboard, Login, Register, Error };
+export { Landing, Dashboard, Register, Error };
